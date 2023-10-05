57a739b758a8453a9cc2e1ac416983b0e07801e1
Daniel Arteaga <71586064+D4nyArt@users.noreply.github.com>
2023-10-05 07:17:10 -0600

    Update Bitacora.h
    Const pariables removed

--- Bitacora.h
+++ Bitacora.h
@@ -6,19 +6,19 @@ using namespace std;
 class Bitacora {
 public:
     // Constructor para crear una Bitacora vacia
-    Bitacora(const vector<string> campos, const string campoClave>);
+    Bitacora(vector<string> campos, string campoClave>);
 
     // Carga un registro individual a la Bitacora
-    void CargaIndividual(const vector<string> registro);
+    void CargaIndividual(vector<string> registro);
 
     // Carga varios registros desde un archivo
-    void CargaLotes(const string nombreArchivo);
+    void CargaLotes(string nombreArchivo);
 
     // Ordena la Bitacora por un campo clave
-    bool Ordena(const string campoClave);
+    bool Ordena(string campoClave);
 
     // Consulta registros en la Bitacora dentro de un rango
-    vector<vector<string>> Consulta(const string campoClave, const string desde, const string hasta);
+    vector<vector<string>> Consulta(string campoClave, string desde, string hasta);
 
     // Limpia la Bitacora
     void Limpiar();
