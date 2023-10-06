aa685ce63da74f787eb77d1a339c35767f81a0f4
Daniel Arteaga <71586064+D4nyArt@users.noreply.github.com>
2023-10-06 16:02:33 -0600

    Added "ShowConsultaResult" function

--- prueba_implementacion.cpp
+++ prueba_implementacion.cpp
@@ -1,9 +1,19 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 #include "Bitacora.h"
 
 using namespace std;
+
+void showConsultaResult(vector<vector<string>> consultaRes){
+    for(int i=0; i<consultaRes.size(); i++){
+        for(int j=0; j<consultaRes[i].size(); j++){
+            cout << consultaRes[i][j] << " ";
+        }        
+        cout << '\n';
+    }
+}
 
 int main(int argc, char const *argv[]) {
     Bitacora failed_logins();
