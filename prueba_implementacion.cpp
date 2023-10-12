66ca223e6ac9d95c6ab645517851df012d988e3e
Daniel Arteaga <71586064+D4nyArt@users.noreply.github.com>
2023-10-11 21:53:11 -0600

    Re-Added showConsultaResult

--- prueba_implementacion.cpp
+++ prueba_implementacion.cpp
@@ -1,10 +1,9 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <regex>
 #include "Bitacora.cpp"
 //#include "Bitacora.h"
-
-using namespace std;
 
 void showConsultaResult(vector<vector<string>> consultaRes) {
     for (int i = 0; i < consultaRes.size(); i++) {
@@ -15,15 +14,17 @@ void showConsultaResult(vector<vector<string>> consultaRes) {
     }
 }
 
+using namespace std;
+
 int main() {
 	Bitacora B({"Mes", "Dia", "Hora", "IP", "Falla"}, "Dia");
-	Bitacora BC({"Mes", "Dia", "Hora", "IP", "Falla"}, "Dia");
 	int choice;
-	vector<string> registro(5);
-	string desde, hasta, lote;
+	vector<vector<string>> registros;
+	string desde, hasta, nomArchivo;
 	bool bitacora_creada=false;
 	bool bitacora_ordenada=false;
-	do{
+	bool bitacora_limpia=true;
+	while(true){
 		cout<<"-------------------------------------------------"<<endl;
 		cout<<"			Menu			"<<endl;
 		cout<<"-------------------------------------------------"<<endl;
@@ -33,90 +34,110 @@ int main() {
 	
 	    switch (choice){
 	    case 0:
-            break;
+            return 0;
 	    case 1:
 			B = Bitacora({"Mes", "Dia", "Hora", "IP", "Falla"}, "Dia");
-            bitacora_creada = true;
+            bitacora_creada=true;
+            bitacora_limpia=true;
 			cout<<"Bitacora creada"<<endl;
 			break;
 		case 2:
 			if(!bitacora_creada){
                 cout<<"La bitacora no ha sido creada todavia"<<endl;
             } else{
-				cout<<"Indica el mes (coloca las primeras 3 letras): "<<endl;
+            	vector<string> registro(5);
+				cout<<"Indica el mes (coloca las primeras 3 letras en minuscula): ";
+				cin.ignore();
 				cin>>registro[0];
-				while(registro[0].size()!=3){
-			        cout<<"El mes debe tener exactamente 3 letras. Ingrese nuevamente: ";
+				while(registro[0].size()!=3 || (registro[0] != "ene" && registro[0] != "feb" && registro[0] != "mar" && registro[0] != "abr" && registro[0] != "may" && registro[0] != "jun" && registro[0] != "jul" && registro[0] != "ago" && registro[0] != "sep" && registro[0] != "oct" && registro[0] != "nov" && registro[0] != "dic")){
+			        cout<<"El mes debe tener exactamente 3 letras en minuscula. Ingrese nuevamente: ";
 			        cin.ignore();
 			        cin>>registro[0];
 			    }
-				cout<<"Indica el dia: "<<endl;
+				cout<<"Indica el dia: ";
 				cin>>registro[1];
 				while(stoi(registro[1])<1 || stoi(registro[1])>31){
 			        cout<<"El dia debe estar en el rango de 1 a 31. Ingrese nuevamente: ";
-			        cin.ignore();
 			        cin>>registro[1];
 			    }
-				cout<<"Indica la hora (hh:mm:ss): "<<endl;
+				cout<<"Indica la hora (hh:mm:ss): ";
 				cin>>registro[2];
 				while (registro[2].size()!=8 || registro[2][2]!=':' || registro[2][5]!=':' || stoi(registro[2].substr(0, 2))>23 || stoi(registro[2].substr(3, 2))>59 || stoi(registro[2].substr(6, 2))>59){
-			        cout<<"Asegúrese de cumplir con el formato de hora e ingrese nuevamente: ";
+			        cout<<"Asegurese de cumplir con el formato de hora e ingrese nuevamente: ";
 			        cin.ignore();
 			        cin>>registro[2];
 			    }
-				cout<<"Indica la direccion IP (en formato xxx.xx.xxx.xx:xxxx): "<<endl;
+				cout<<"Indica la direccion IP (en formato xxx.xx.xxx.xx:xxxx): ";
 				cin>>registro[3];
-				cout<<"Indica la razon de falla: "<<endl;
-				cin>>registro[4];
+				regex regexIP("^(\\d{1,3}\\.){3}\\d{1,3}:(\\d{1,5})$");
+			    while (!regex_match(registro[3], regexIP)) {
+			        cout << "Asegurese de cumplir con el formato de la direccion IP e ingrese nuevamente: ";
+			        cin.ignore();
+			        cin >> registro[3];
+			    }
+				cout<<"Indica la razon de falla: ";
+				cin.ignore();
+				getline(cin, registro[4]);
 				B.CargaIndividual(registro);
+				bitacora_limpia=false;
+				cout<<"Carga individual exitosa"<<endl;
 			}
 			break;
 		case 3:
 			if(!bitacora_creada){
                 cout<<"La bitacora no ha sido creada todavia"<<endl;
             } else{
-				cout<<"Ingrese el lote completo (Mes Dia Hora IP Falla): "<<endl;
-				cin.ignore();
-				getline(cin, lote);
-				B.CargaLotes(lote);
-		        cout<<"Lote cargado"<<endl;
-			}
+            	vector<string> registro(5);
+				cout<<"Ingrese el nombre del archivo con los registros a cargar: ";
+				cin>>nomArchivo;
+		        B.CargaLotes(nomArchivo);
+		        bitacora_limpia=false;
+	        	cout<<"Lote cargado"<<endl;
+			    }
 	    	break;
 		case 4:
 			if(!bitacora_creada){
                 cout<<"La bitacora no ha sido creada todavia"<<endl;
+            } 
+			if(bitacora_limpia){
+                cout<<"No hay registros para ordenar"<<endl;
             } else{
 				B.Ordena("ordenamiento.txt");
 				bitacora_ordenada=true;
-				cout<<"Ordenamiento exitoso"<<endl; 
-				/*if(!B.Bitacora({"Mes", "Dia", "Hora", "IP", "Falla"}, "Mes")){
-					cout<<"No se puede ordenar si no existe un registro previo"<<endl;
-				}*/
-			}
+				cout<<"Ordenamiento exitoso"<<endl;
 			break;
 		case 5:
 			if(!bitacora_creada){
                 cout<<"La bitacora no ha sido creada todavia"<<endl;
             } 
-			if(!bitacora_ordenada){
+			/*if(!bitacora_ordenada){
                 cout<<"La bitacora no ha sido ordenada todavia"<<endl;
+            } */
+			if(bitacora_limpia){
+                cout<<"No hay registros para consultar"<<endl;
             } else{
-				cout<<"Indique el mes inicial de la consulta: "<<endl;
-				cin>>desde;
-				cout<<"Indique el mes final de la consulta: "<<endl;
-				cin>>hasta;
-				BC.CargaLotes("ordenamiento.txt");
-				showConsultaResult(BC.Consulta(desde, hasta));
+					cout<<"Indique el dia inicial de la consulta: ";
+					cin>>desde;
+					cout<<"Indique el dia final de la consulta: ";
+					cin>>hasta;
+					B.CargaLotes("ordenamiento.txt");
+					B.Consulta(desde, hasta);
+					showConsultaResult(registros);
 			}
 			break;
 		case 6:
-			B.Limpiar();
-			cout<<"Bitacora limpia :)"<<endl;
+			if(!bitacora_creada){
+                cout<<"La bitacora no ha sido creada todavia"<<endl;
+            } else{
+				B.Limpiar();
+				bitacora_limpia=true;
+				cout<<"Bitacora limpia :)"<<endl;
+			}
 			break;
 		default:
-	        cout<<"Numero no valido, intenta de nuevo"<<endl;
+	        cout<<"Esa opcion no se encuentra en el menu, intenta de nuevo"<<endl;
 	        break;
 		}
-	} while (choice!=0);
+	}
 	return 0;
 }
